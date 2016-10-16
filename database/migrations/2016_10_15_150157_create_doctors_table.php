19c32e767262f4187235fa54b92e74b507b3dd0d
Michal Czarnecki <michal.boguslaw.czarnecki@gmail.com>
2016-10-16 13:53:00 +0200

    Initial commit

---
+++ database/migrations/2016_10_15_150157_create_doctors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name', 30);
+            $table->string('last_name', 30);
+            $table->string('specialization', 30);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
