89a7205bdd01d2a28ede71a6ea018b80bbba03cf
Michal Czarnecki <michal.boguslaw.czarnecki@gmail.com>
2016-10-20 22:15:22 +0200

    Created migration for treatments table creation

---
+++ database/migrations/2016_10_20_175700_create_treatments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTreatmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::enableForeignKeyConstraints();
+
+        Schema::create('treatments', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->timestamp('date_of_treatment');
+            $table->decimal('price');
+            $table->string('type_visit', 30);
+            $table->bigInteger('doctor_id')->unsigned();
+            $table->bigInteger('patient_id')->unsigned();
+
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('treatments');
+    }
+}
