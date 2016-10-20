5eccb394f61ce330af74c2009c85afca1685b626
Michal Czarnecki <michal.boguslaw.czarnecki@gmail.com>
2016-10-20 22:20:30 +0200

    Created migration for referrals table creation

---
+++ database/migrations/2016_10_20_201839_create_referrals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReferralsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('referrals', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->string('description', 60);
+            $table->bigInteger('doctor_id')->unsigned();
+            $table->bigInteger('patient_id')->unsigned();
+
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('referrals');
+    }
+}
