c5873e936c21a96214d5aca04aecee986e228cb6
Michal Czarnecki <michal.boguslaw.czarnecki@gmail.com>
2016-10-20 19:56:36 +0200

    Migation of patients table

---
+++ database/migrations/2016_10_20_175331_create_patients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name', 30);
+            $table->string('last_name', 30);
+            $table->bigInteger('pesel');
+            $table->timestamp('date_of_birth');
+            $table->string('address', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
